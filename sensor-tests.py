f6446abf0c9008d7733dd6931d046208a807eb3e
Darrekt <dl5215@ic.ac.uk>
2020-01-28 19:34:27 +0000

    +Initial lux calculations

--- sensor-tests.py
+++ sensor-tests.py
@@ -16,6 +16,15 @@ while True:
     ch0 = ch0_lo | (ch0_hi << 8)
     ch1 = ch1_lo | (ch1_hi << 8)
     
-    coeff = float(ch1) / float(ch0)   
-    if (0 < coeff and coeff <= 0.5)
-        lux = 0.0304 * ch0 - 0.062 * ch0 * ((ch1/ch0))
+    coeff = float(ch1) / float(ch0)
+    lux = 0.0
+    if 0 < coeff and coeff <= 0.5: 
+        lux = 0.0304 * ch0 - 0.062 * ch0 * (pow(ch1/ch0), 1.4)
+    elif (0.5 < coeff and coeff <= 0.61):
+        lux = 0.0224 * ch0 - 0.031 * ch1
+    elif (0.61 < coeff and coeff <= 0.8):
+        lux = 0.0128 * ch0 - 0.0153 * ch1
+    elif (0.8 < coeff and coeff <= 1.3):
+        lux = 0.00146 * ch0 - 0.00112 * ch1
+
+    print(lux)

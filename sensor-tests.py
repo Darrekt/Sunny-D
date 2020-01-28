d99546e9a3c022f181af867f3cb1ce8d00002acf
= <=>
2020-01-28 19:23:53 +0000

    Start lux calculations

--- sensor-tests.py
+++ sensor-tests.py
@@ -1,4 +1,5 @@
 import smbus
+import math
 
 # Initialisation
 bus = smbus.SMBus(1)
@@ -6,8 +7,15 @@ bus.write_byte_data(0x39, 0x0, 0x3)
 
 # Polling loop
 while True:
-    CH0_Lo = bus.read_byte_data(0x39, 0xC)
-    CH0_Hi = bus.read_byte_data(0x39, 0xD)
+    ch0_lo = bus.read_byte_data(0x39, 0xC) & 0xFF
+    ch0_hi = bus.read_byte_data(0x39, 0xD) & 0xFF
 
-    CH1_Lo = bus.read_byte_data(0x39, 0xE)
-    CH1_Hi = bus.read_byte_data(0x39, 0xF)
+    ch1_lo = bus.read_byte_data(0x39, 0xE) & 0xFF
+    ch1_hi = bus.read_byte_data(0x39, 0xF) & 0xFF
+
+    ch0 = ch0_lo | (ch0_hi << 8)
+    ch1 = ch1_lo | (ch1_hi << 8)
+    
+    coeff = float(ch1) / float(ch0)   
+    if (0 < coeff and coeff <= 0.5)
+        lux = 0.0304 * ch0 - 0.062 * ch0 * ((ch1/ch0))

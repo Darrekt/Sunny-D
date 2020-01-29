6d2ca893aaa176eac163570307a0839a6a89a15d
= <=>
2020-01-29 00:21:28 +0000

    Added intial sensor test file, NOTE: register need to be accessed with MSB = 1

--- sensor-tests.py
+++ sensor-tests.py
@@ -1,30 +1,40 @@
 import smbus
 import math
+import time
 
 # Initialisation
 bus = smbus.SMBus(1)
-bus.write_byte_data(0x39, 0x0, 0x3)
+bus.write_byte_data(0x39, 0x80, 0x03)
+print(hex(bus.read_byte_data(0x39, 0x80)))
+gain = int(input("Enter 1 for 16x gain: "))
+if gain:
+    bus.write_byte_data(0x39, 0x81, 0x10)
+else:
+    bus.write_byte_data(0x39, 0x81, 0x00)
+
+print(hex(bus.read_byte_data(0x39, 0x81)))
 
 # Polling loop
 while True:
-    ch0_lo = bus.read_byte_data(0x39, 0xC) & 0xFF
-    ch0_hi = bus.read_byte_data(0x39, 0xD) & 0xFF
+    time.sleep(0.3)
+    ch0_lo = bus.read_byte_data(0x39, 0x8C) & 0xFF
+    ch0_hi = bus.read_byte_data(0x39, 0x8D) & 0xFF
 
-    ch1_lo = bus.read_byte_data(0x39, 0xE) & 0xFF
-    ch1_hi = bus.read_byte_data(0x39, 0xF) & 0xFF
+    ch1_lo = bus.read_byte_data(0x39, 0x8E) & 0xFF
+    ch1_hi = bus.read_byte_data(0x39, 0x8F) & 0xFF
 
     ch0 = ch0_lo | (ch0_hi << 8)
     ch1 = ch1_lo | (ch1_hi << 8)
-    
-    coeff = float(ch1) / float(ch0)
-    lux = 0.0
-    if 0 < coeff and coeff <= 0.5: 
-        lux = 0.0304 * ch0 - 0.062 * ch0 * (pow(ch1/ch0), 1.4)
-    elif (0.5 < coeff and coeff <= 0.61):
-        lux = 0.0224 * ch0 - 0.031 * ch1
-    elif (0.61 < coeff and coeff <= 0.8):
-        lux = 0.0128 * ch0 - 0.0153 * ch1
-    elif (0.8 < coeff and coeff <= 1.3):
-        lux = 0.00146 * ch0 - 0.00112 * ch1
+    if(ch0 != 0): 
+        coeff = float(ch1) / float(ch0)
+        lux = 0.0
+        if 0 < coeff and coeff <= 0.5: 
+            lux = (0.0304 * ch0) - (0.062 * ch0 * (pow((ch1/ch0), 1.4)))
+        elif (0.5 < coeff and coeff <= 0.61):
+            lux = (0.0224 * ch0) - (0.031 * ch1)
+        elif (0.61 < coeff and coeff <= 0.8):
+            lux = (0.0128 * ch0) - (0.0153 * ch1)
+        elif (0.8 < coeff and coeff <= 1.3):
+            lux = (0.00146 * ch0) - (0.00112 * ch1)
 
-    print(lux)
+        print(str(lux) + '\t, ' + str(ch0) + '\t, '+ str(ch1))
